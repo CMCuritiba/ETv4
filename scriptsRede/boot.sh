3e90b2966abd6e2153c983580a5b10815afe8f7b
System User <lls2019@dtic29.cmc.pr.gov.br>
2020-11-24 10:41:55 -0300

    Testando se tem alguma impressora instalada antes de tentar instalar uma

--- scriptsRede/boot.sh
+++ scriptsRede/boot.sh
@@ -5,7 +5,16 @@
 logger "[$0] Iniciando script de configuracoes."
 
 # Instala impressoras por hostname
-/mnt/suporte/etv4/scripts/instala-impressora.sh
+
+#Variável print é vazia se alguma impressora estiver instalada
+print=`lpstat -t | cut -f1 -d ' ' | grep -i 'nenhum'`
+
+#Se print não for uma variável vazia, instala impressora
+if [ ! -z  $print ]; then
+	/mnt/suporte/etv4/scripts/instala-impressora.sh
+#else
+	#echo "Uma ou mais impressoras já estão instaladas"
+fi
 
 # Placeholder para hotfixes
 # /mnt/suporte/etv4/scripts/hotfixes.sh
